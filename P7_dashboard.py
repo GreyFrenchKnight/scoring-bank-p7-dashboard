defcd3cc3325f413b3ed9ded128cf27b70a824bf
Grey French Knight <dischanthony@gmail.com>
2022-05-26 17:36:09 +0200

    Update for deployment on heroku

--- P7_dashboard.py
+++ P7_dashboard.py
@@ -1,17 +1,11 @@
-#import joblib
 import streamlit as st
-
 import streamlit.components.v1 as components
 import pandas as pd
 import numpy as np
 import shap
-#import matplotlib.pyplot as plt
-#import plotly.graph_objects as go
-#from sklearn import model_selection
-#from sklearn.neighbors import NearestNeighbors
-#import seaborn as sns
-#import lightgbm
-#from matplotlib.image import imread
+
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 import requests
 import io
@@ -34,50 +28,51 @@ first_page = 'Tous les clients'
 second_page = 'Score Client'
 third_page = 'Comparaison Clients'
 
-PAGES = [
-    first_page,
-    second_page,
-    third_page
-]
+DEFAULT = '< PICK A CLIENT SK_ID_CURR >'
 
-# Initialization
-if 'selected_page' not in st.session_state:
-    st.session_state['selected_page'] = first_page
-if 'selected_page_index' not in st.session_state:
-    st.session_state['selected_page_index'] = 0        
+def readFileToList(filepath, label):
+    # opening the file in read mode
+    _file = open(filepath, 'r')
+    # reading the file
+    data = _file.read()
+    # replacing end splitting the text 
+    # when newline ('\n') is seen.
+    data_list = data.split("\n")
+    print("\n" + label, data_list)
+    _file.close()
     
-"st.session_state object", st.session_state
+    return data_list
+
+api_url = readFileToList('conf/api.txt', 'CONF API url:')[0]
 
 def load_data():
     '''Fonction chargeant et calculant les données nécessaires au dashboard.
     Ne prend pas de paramètres en entrée
     '''
     # Chargement des noms de colonnes pour la prédiction du model
-    r = requests.get('http://127.0.0.1:8000/features_for_model_prediction')
+    r = requests.get(api_url + 'features_for_model_prediction')
     features_for_model_prediction = r.json()
     print("\nFeatures for model prediction:", features_for_model_prediction)
     
     # Chargement des noms de colonnes pour l'affichage du tableau
-    r = requests.get('http://127.0.0.1:8000/features_for_dashboard_table')
+    r = requests.get(api_url + 'features_for_dashboard_table')
     features_for_dashboard_table = r.json()
     print("\nFeatures for dashboard main table:", features_for_dashboard_table)
     
     # Chargement des données de test
-    r = requests.get('http://127.0.0.1:8000/clients')
+    r = requests.get(api_url + 'clients')
     db_test = pd.read_json(str(r.json()).replace("'", '"'))
     db_test = db_test.reset_index(drop=True)
     
-    # Initialization
-    if 'selected_client' not in st.session_state:
-        st.session_state['selected_client'] = db_test['SK_ID_CURR'][0]
-    
     # Chargement des SHAP values
-    r = requests.get('http://127.0.0.1:8000/shap_shap_values')
+    r = requests.get(api_url + 'shap_shap_values')
     inmemoryfile = io.BytesIO(r.content)
-    shap_values = np.genfromtxt(inmemoryfile, dtype=float, delimiter=" ")
+    # shap_values = np.genfromtxt(inmemoryfile, dtype=float, delimiter=" ")
+    data = np.load(inmemoryfile)
+    shap_values = data['shap_values']    
     print("\nSHAP SHAP value:", shap_values)
     
-    r = requests.get('http://127.0.0.1:8000/shap_expected_value')
+    r = requests.get(api_url + 'shap_expected_value')
     exp_value = r.json()
     print("\nSHAP expected value:", exp_value)
     
@@ -92,20 +87,33 @@ def load_data():
 
 def get_client(db_test):
     """Sélection d'un client via une selectbox"""
-    client = st.sidebar.selectbox('Client', db_test['SK_ID_CURR'], key="selected_client")
-    idx_client = db_test.index[db_test['SK_ID_CURR'] == client][0]
-    return client, idx_client
+
+    selectbox_values = [''] + list(db_test['SK_ID_CURR'].copy())    
+    client = st.sidebar.selectbox(label='', options=selectbox_values, index=0, format_func=lambda x: DEFAULT if x == '' else x, key="selected_client")
+    
+    if client:
+        idx_client = db_test.index[db_test['SK_ID_CURR'] == client][0]
+        return client, idx_client
+    else:        
+        return None, -1        
 
 
 def infos_client(db_test, client, idx_client):
     """Affichage des infos du client sélectionné dans la barre latérale"""
-    st.sidebar.markdown("**ID client:** " + str(client))
-    st.sidebar.markdown("**Sexe:** " + db_test.loc[idx_client,'CODE_GENDER'])
-    st.sidebar.markdown("**Statut familial:** " + db_test.loc[idx_client,'NAME_FAMILY_STATUS'])
-    st.sidebar.markdown("**Enfants:** " + str(db_test.loc[idx_client,'CNT_CHILDREN']))
-    st.sidebar.markdown("**Age:** " + str(db_test.loc[idx_client,'YEARS_BIRTH']))	
-    st.sidebar.markdown("**Statut pro.:** " + db_test.loc[idx_client,'NAME_INCOME_TYPE'])
-    st.sidebar.markdown("**Niveau d'études:** " + db_test.loc[idx_client,'NAME_EDUCATION_TYPE'])
+    if client:
+        st.sidebar.markdown("**ID client:** " + str(client))        
+        st.sidebar.markdown("**Genre:** " + db_test.loc[idx_client, 'CODE_GENDER'])
+        st.sidebar.markdown("**Âge:** " + db_test.loc[idx_client, 'YEARS_BIRTH'].astype(str))
+        st.sidebar.markdown("**Statut familial:** " + db_test.loc[idx_client, 'NAME_FAMILY_STATUS'])
+        st.sidebar.markdown("**Enfants:** " + db_test.loc[idx_client, 'CNT_CHILDREN'].astype(str))
+        st.sidebar.markdown("**Niveau d'études:** " + db_test.loc[idx_client, 'NAME_EDUCATION_TYPE'])
+        st.sidebar.markdown("**Véhiculé:** " + db_test.loc[idx_client, 'FLAG_OWN_CAR'])
+        st.sidebar.markdown("**Propriétaire:** " + db_test.loc[idx_client, 'FLAG_OWN_REALTY'])
+        st.sidebar.markdown("**Type de logement:** " + db_test.loc[idx_client, 'NAME_HOUSING_TYPE'])
+        st.sidebar.markdown("**Statut pro.:** " + db_test.loc[idx_client, 'NAME_INCOME_TYPE'])
+        st.sidebar.markdown("**Revenus totaux:** " + db_test.loc[idx_client, 'AMT_INCOME_TOTAL'].astype(str))
+        st.sidebar.markdown("**Crédit:** " + db_test.loc[idx_client, 'AMT_CREDIT'].astype(str))
+        st.sidebar.markdown("**Annuité:** " + db_test.loc[idx_client, 'AMT_ANNUITY'].astype(str))
     
 
 def tab_client(db_test, features_for_dashboard_table):
@@ -114,21 +122,20 @@ def tab_client(db_test, features_for_dashboard_table):
     permettant une recherche plus précise.
     Le paramètre est le dataframe des clients
     '''
-    st.title(main_title)
-    st.subheader(first_page)
+    st.title(first_page)
     
     row0_1,row0_spacer2,row0_2,row0_spacer3,row0_3,row0_spacer4,row0_4,row0_spacer5,row0_5,row0_spacer6 = st.columns([1,.1,1,.1,1,.1,1,.1,1,.1])
 
 	#Définition des filtres via selectbox    
-    sex = row0_1.selectbox("Genre",['All']+db_test['CODE_GENDER'].unique().tolist())
-    age = row0_2.selectbox("Age",['All']+(np.sort(db_test['YEARS_BIRTH'].unique()).astype(str).tolist()))
-    fam = row0_3.selectbox("Statut familial",['All']+db_test['NAME_FAMILY_STATUS'].unique().tolist())
-    child = row0_4.selectbox("Enfants",['All']+(np.sort(db_test['CNT_CHILDREN'].unique()).astype(str).tolist()))
-    stud = row0_5.selectbox("Niveau d'études",['All']+db_test['NAME_EDUCATION_TYPE'].unique().tolist())
-    car = row0_1.selectbox("Véhiculé",['All']+db_test['FLAG_OWN_CAR'].unique().tolist())
-    realty = row0_2.selectbox("Propriétaire",['All']+db_test['FLAG_OWN_REALTY'].unique().tolist())
-    housing = row0_3.selectbox("Type de logement",['All']+db_test['NAME_HOUSING_TYPE'].unique().tolist())
-    pro = row0_4.selectbox("Source de revenu",['All']+db_test['NAME_INCOME_TYPE'].unique().tolist())
+    sex = row0_1.selectbox("Genre", ['All'] + db_test['CODE_GENDER'].unique().tolist())
+    age = row0_2.selectbox("Age", ['All'] +(np.sort(db_test['YEARS_BIRTH'].unique()).astype(str).tolist()))
+    fam = row0_3.selectbox("Statut familial", ['All'] + db_test['NAME_FAMILY_STATUS'].unique().tolist())
+    child = row0_4.selectbox("Enfants", ['All'] +(np.sort(db_test['CNT_CHILDREN'].unique()).astype(str).tolist()))
+    stud = row0_5.selectbox("Niveau d'études", ['All'] + db_test['NAME_EDUCATION_TYPE'].unique().tolist())
+    car = row0_1.selectbox("Véhiculé", ['All'] + db_test['FLAG_OWN_CAR'].unique().tolist())
+    realty = row0_2.selectbox("Propriétaire", ['All'] + db_test['FLAG_OWN_REALTY'].unique().tolist())
+    housing = row0_3.selectbox("Type de logement", ['All'] + db_test['NAME_HOUSING_TYPE'].unique().tolist())
+    pro = row0_4.selectbox("Source de revenu", ['All'] + db_test['NAME_INCOME_TYPE'].unique().tolist())
     
     row0_1,row0_spacer2,row0_2,row0_spacer3,row0_3,row0_spacer4 = st.columns([2,.1,2,.1,2,.1])
     total_income = row0_1.slider(label="Revenus totaux", min_value=db_test['AMT_INCOME_TOTAL'].min(), max_value=db_test['AMT_INCOME_TOTAL'].max(), value=(float(db_test['AMT_INCOME_TOTAL'].min()), float(db_test['AMT_INCOME_TOTAL'].max())), step=1000.)
@@ -162,18 +169,68 @@ def tab_client(db_test, features_for_dashboard_table):
     
     st.dataframe(db_display)
     st.markdown("**Total clients correspondants:** " + str(len(db_display)))
-   
-    # allows user to filter then select one row
-    selected_indices = st.multiselect('Select row:', db_test['SK_ID_CURR'])
     
-    # change to second page with currently selected client row
-    if len(selected_indices) == 1:
-        print(selected_indices[0])
-        if st.button(second_page):
-            st.session_state['selected_page_index'] = 1   
-            st.session_state['selected_page'] = second_page
-            st.session_state['selected_client'] = selected_indices[0]
-            st.experimental_rerun()    
+    
+def display_charts(df, idx_client=None):
+    """Affichae des graphes de comparaison pour le client sélectionné """
+    row1_1, row1_2, row1_3 = st.columns(3)
+    st.write('')
+    row2_10, row2_2, row2_3 = st.columns(3)
+
+    chart_kde("Répartition de l'age", row1_1, df, 'YEARS_BIRTH', idx_client)
+    chart_kde("Répartition des revenus", row1_2, df, 'AMT_INCOME_TOTAL', idx_client)
+    chart_bar("Répartition du nombre d'enfants", row1_3, df, 'CNT_CHILDREN', idx_client)
+
+    chart_bar("Répartition du statut professionel", row2_10, df, 'NAME_INCOME_TYPE', idx_client)
+    chart_bar("Répartition du niveau d'études", row2_2, df, 'NAME_EDUCATION_TYPE', idx_client)
+    chart_bar("Répartition du type de logement", row2_3, df, 'NAME_HOUSING_TYPE', idx_client)
+    
+
+def chart_kde(title, row, df, col, idx_client=None):
+    """Définition des graphes KDE avec une ligne verticale indiquant la position du client"""
+    with row:
+        st.subheader(title)
+        fig, ax = plt.subplots()
+        sns.kdeplot(df.loc[df['TARGET']==0, col], color='green',  label = 'Target == 0')
+        sns.kdeplot(df.loc[df['TARGET']==1, col], color='red',  label = 'Target == 1')
+        if idx_client >= 0:
+            plt.axvline(x=df.loc[idx_client, col], ymax=0.95, color='black')
+        plt.legend()
+        st.pyplot(fig)
+        
+
+def chart_bar(title,  row,  df,  col,  idx_client=None):
+    """Définition des graphes barres avec une ligne horizontale indiquant la position du client"""
+    with row:
+        st.subheader(title)
+        fig, ax = plt.subplots()
+        data=df[['TARGET', col]]
+        if data[col].dtypes!='object':
+            data[col]=data[col].astype('str')
+
+            data1=round(data[col].loc[data['TARGET']==1].value_counts()/data[col].loc[data['TARGET']==1].value_counts().sum()*100, 2)
+            data0=round(data[col].loc[data['TARGET']==0].value_counts()/data[col].loc[data['TARGET']==0].value_counts().sum()*100, 2)
+            data=pd.concat([pd.DataFrame({"Pourcentage":data0, 'TARGET':0}), pd.DataFrame({'Pourcentage':data1, 'TARGET':1})]).reset_index().rename(columns={'index':col})
+            sns.barplot(data=data, x='Pourcentage', y=col, hue='TARGET', palette=['green', 'red'], order=sorted(data[col].unique()));
+
+            data[col]=data[col].astype('int64')
+
+            if idx_client >= 0:
+                plt.axhline(y=sorted(data[col].unique()).index(df.loc[idx_client, col]), xmax=0.95, color='black', linewidth=4)
+                
+            st.pyplot(fig)
+        else:
+
+            data1=round(data[col].loc[data['TARGET']==1].value_counts()/data[col].loc[data['TARGET']==1].value_counts().sum()*100, 2)
+            data0=round(data[col].loc[data['TARGET']==0].value_counts()/data[col].loc[data['TARGET']==0].value_counts().sum()*100, 2)
+            data=pd.concat([pd.DataFrame({"Pourcentage":data0, 'TARGET':0}), pd.DataFrame({'Pourcentage':data1, 'TARGET':1})]).reset_index().rename(columns={'index':col})
+            sns.barplot(data=data, x='Pourcentage', y=col, hue='TARGET', palette=['green', 'red'], order=sorted(data[col].unique()));
+
+            if idx_client >= 0:
+                plt.axhline(y=sorted(data[col].unique()).index(df.loc[idx_client, col]), xmax=0.95, color='black', linewidth=4)
+                
+            st.pyplot(fig)
+            
     
 def filter(df, col, value):
     '''Fonction pour filtrer le dataframe selon la colonne et la valeur définies'''
@@ -191,18 +248,14 @@ def filter(df, col, value):
 
 def comparaison(db_test, idx_client):
     """Fonction principale de l'onglet 'Comparaison clientèle' """
-    st.title(main_title)
-    st.subheader(third_page)
+    st.title(third_page)
     
-    print("\nTODO")
-    
-    print("\n")
+    display_charts(db_test, idx_client)
     
     
 def score_viz(db_test, idx_client, features_for_model_prediction, shap_values, exp_value):
     """Fonction principale de l'onglet 'Score visualisation' """
-    st.title(main_title)
-    st.subheader(second_page)
+    st.title(second_page)
     
     # Prediction and SHAP values
     
@@ -211,7 +264,7 @@ def score_viz(db_test, idx_client, features_for_model_prediction, shap_values, e
     jsoned = json.dumps(client_features_for_model_prediction)
     
     # POST
-    r = requests.post(url='http://127.0.0.1:8000/loan_repayment', data=jsoned)
+    r = requests.post(url=api_url + 'loan_repayment', data=jsoned)
     
     # Response
     response = json.loads(r.json())
@@ -240,32 +293,25 @@ def st_shap(plot, height=None):
 	components.html(shap_html, height=height)
 
 
-def page_changed():
-    st.session_state.selected_page_index = PAGES.index(st.session_state.selected_page)
-    print('current page index', st.session_state.selected_page_index)
-
- 
 def main():
     """Fonction principale permettant l'affichage de la fenêtre latérale avec les 3 onglets.
     """
     db_test, logo, features_for_model_prediction, features_for_dashboard_table, shap_values, exp_value = load_data()
     st.sidebar.image(logo)
-    st.sidebar.write('')
-    st.sidebar.write('')
-
-    st.sidebar.title('Menu')
-    st.sidebar.radio(label="", options=PAGES, index=st.session_state.selected_page_index, key="selected_page", on_change=page_changed)
-        
-    if st.session_state.selected_page == first_page:
-        tab_client(db_test, features_for_dashboard_table)
-    if st.session_state.selected_page == second_page:
-        client,idx_client=get_client(db_test)
-        infos_client(db_test, client, idx_client)
+    st.sidebar.title('Sélection Client')
+    
+    st.title(main_title)
+    tab_client(db_test, features_for_dashboard_table)
+    client, idx_client = get_client(db_test)
+    infos_client(db_test, client, idx_client)
+    comparaison(db_test, idx_client)
+    
+    st.caption("<font color=green>**Connecté à l\'API :**</font>", True)
+    st.write(api_url)
+    
+    if idx_client >= 0:
         score_viz(db_test, idx_client, features_for_model_prediction, shap_values, exp_value)
-    if st.session_state.selected_page == third_page:
-        client,idx_client=get_client(db_test)
-        infos_client(db_test, client, idx_client)
-        comparaison(db_test, idx_client)
+        
 
 if __name__ == '__main__':
     main()
--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # scoring-bank-p7-dashboard
 Dashboard du projet P7 Scoring Bank
 
-#### Step 1. Create a new app on Heroku "scoring-bank-p7"
+#### Step 1. Create a new app on Streamlit/Heroku "scoring-bank-p7-dashboard"
 
 #### Step 2. Initiate Project Folder on Local PC
 * Test before commit
 * [any folder pathname] > cd [project folder pathname]
 * [project folder pathname] > python -m venv venv
 * [project folder pathname] > CALL venv/Scripts/activate.bat
-* [project folder pathname] > c:\users\disch\documents\github\scoring-bank-p7-dashboard\venv\scripts\python.exe -m pip install --upgrade pip
-* [project folder pathname] > pip install numpy pandas lightgbm streamlit sklearn uvicorn gunicorn joblib shap matplotlib seaborn plotly
+* [project folder pathname] > python -m pip install --upgrade pip
+* [project folder pathname] > pip install numpy pandas streamlit uvicorn gunicorn shap matplotlib seaborn
 * [project folder pathname] > pip freeze > requirements.txt
 
 #### Step 3. Initiate GIT in the project folder
@@ -17,9 +17,10 @@ Dashboard du projet P7 Scoring Bank
 * [project folder pathname] > git add .
 * [project folder pathname] > git commit -m "first commit"
 * [project folder pathname] > heroku login
-* [project folder pathname] > heroku git:remote -a scoring-bank-p7
+* [project folder pathname] > heroku git:remote -a scoring-bank-p7-dashboard
 * [project folder pathname] > git push heroku master
 
 #### Step 4. Testing application
-* accessing app https://scoring-bank-p7.herokuapp.com/
-* checking logs https://dashboard.heroku.com/apps/scoring-bank-p7/logs
+* Heroku dashboard https://dashboard.heroku.com/apps/scoring-bank-p7-dashboard
+* accessing app https://scoring-bank-p7-dashboard.herokuapp.com/
+* checking logs https://dashboard.heroku.com/apps/scoring-bank-p7-dashboard/logs
